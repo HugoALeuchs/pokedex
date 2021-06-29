e91106aa66c5f5d5dd6b30f89283c254f3ffeb7e
Hugo de Andrade Leuchs <hugo.leuchs@biopark.com.br>
2021-06-28 23:30:44 -0300

    Search bar and Filter box

--- pages/header.js
+++ pages/header.js
@@ -10,18 +10,41 @@ function header(props) {
   return (
     <div className={props.loading ? styles.nav : styles.navOpen}>
       <nav className={styles.navbar}>
-        <img src={PokeballNav}></img>
+        <img className={styles.navbarPokeBall} src={PokeballNav}></img>
         <div
           className={styles.search}
           style={{ display: props.loading ? "none" : "flex" }}
         >
           <img className={styles.icon} src={Loupe}></img>
+          <input className={styles.searchText}></input>
         </div>
         <div
           className={styles.filter}
           style={{ display: props.loading ? "none" : "flex" }}
         >
           <img className={styles.icon} src={Filter}></img>
+          <div className={styles.filtersName}>
+          <p>normal</p>
+          <p>fighting</p>
+          <p>fire</p>
+          <p>grass</p>
+          <p>water</p>
+          <p>flying</p>
+          <p>poison</p>
+          <p>ground</p>
+          <p>rock</p>
+          <p>bug</p>
+          <p>ghost</p>
+          <p>steel</p>
+          <p>electric</p>
+          <p>psychic</p>
+          <p>ice</p>
+          <p>dragon</p>
+          <p>dark</p>
+          <p>fairy</p>
+          <p>unknown</p>
+          <p>shadow</p>
+          </div>
         </div>
         <div
           className={styles.games}
